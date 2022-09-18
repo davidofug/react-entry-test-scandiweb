91d1a034b501314985d2f05a4938e2e990011ead
CrucialE <erasmasNgandu@gmail.com>
2022-09-18 08:19:44 +0200

    created the categorylayout component in categoryjs

--- src/webpages/productListing/Category.js
+++ src/webpages/productListing/Category.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import styled from 'styled-components/macro'
+import {COLORS,FONTS} from '../../components/constants'
+
+const CategoryLayout = styled.main`
+box-sizing:border-box;
+background-color: #E5E5E5;
+color:${COLORS.BLACK};
+max-height:1513px;
+max-width:1440px;
+padding-left:100px;
+padding-right:102px;
+
+`
 
 export default class Category extends Component {
   render() {
     return (
+      <CategoryLayout>
       <div>Category</div>
+      </CategoryLayout>
     )
   }
 }
