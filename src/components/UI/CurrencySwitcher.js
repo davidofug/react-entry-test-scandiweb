6d00adecb34ccbafceb869016f46405533c36ff4
CrucialE <erasmasNgandu@gmail.com>
2022-09-21 21:42:39 +0200

    Worked on CurrencySwitcher component and added toggle functionality

--- src/components/UI/CurrencySwitcher.js
+++ src/components/UI/CurrencySwitcher.js
@@ -1,33 +1,91 @@
-
 import React, { Component } from 'react'
-import styled from 'styled-components/macro'
-import USDCurrencyIcon from '../../assets/icons/USDCurrencyIcon.png'
-
-
+import styled,{css} from 'styled-components/macro'
+import ChevrondownIconPath from '../../assets/icons/chevrondown.png'
+import { COLORS,FONTS } from 'components/constants'
 const Wrapper = styled.div`
-  /* display:flex;
-  justify-content:space-between; */
-  /* margin:0 32px; */
-  cursor: pointer;
-`
-// const SelectWrapper = styled.span`
-//   padding:0 2px;
+      position: relative;
+      cursor: pointer;
+      padding:0 10px;
+  `
+const SelectWrapper = styled.span`
+  padding:8px;
 
-// `
-// const CurrencySymbol = styled.span`
-//   font-family:${FONTS.FAMILIES.RALEWAY};
-//   font-weight:${FONTS.WEIGHTS.LARGE};
-//   font-size:${FONTS.SIZES.EIGHTEEN};
-//   line-height:28.8px;
-// `
+`
+ const CurrencySymbol = styled.span`
+    font-family:${FONTS.FAMILIES.RALEWAY};
+    font-weight:${FONTS.WEIGHTS.LARGE};
+    font-size:${FONTS.SIZES.EIGHTEEN};
+    line-height:28.8px;
+`
+const DropdownUl = styled.ul`
+    position:absolute;
+    padding-top:4px;
+    background: #FFF;
+    ${props => {return props.$currencyState ?
+      css`
+        display:block;
+      `:
+      css`
+        display: none;
+      `
+    }};
+ 
+    >li{
+      display:block;
+      list-style-type: none;
+      padding:10px;
+      text-decoration:none;
+      font-family:${FONTS.FAMILIES.RALEWAY};
+      font-size:${FONTS.SIZES.EIGHTEEN};
+      font-weight:${FONTS.WEIGHTS.LARGE};
+      line-height:28.8px;
+      &:hover{
+        background:${COLORS.BACKGROUND.GRAY};
+
+      
+      }
+    }
+        
+`
+  const ChevronIcon = styled.img`
+    font-weight:${FONTS.WEIGHTS.LARGEST};
+    line-height:28.8px;
+    transition:transform 0.2s;
+    &:hover{
+      transform:rotateZ(180deg);
+    }
+`
+
+const CurrencyList = styled.li`
+  display:block;
+`
 export default class CurrencySwitcher extends Component {
+   constructor(){
+    super()
+
+    this.state = {
+      currencySwitch: false
+    }
+    
+   }
+
+  toggleCurrencySwitcher = () => {
+    this.setState({currencySwitch: !this.state.currencySwitch});
+  }
+
   render() {
     
     return (
-    <Wrapper> 
-      
-      <img src = {USDCurrencyIcon} alt ='USD-Currency'/>
-      
+    <Wrapper onClick={this.toggleCurrencySwitcher}>
+        <CurrencySymbol>$</CurrencySymbol>
+      <SelectWrapper>  
+          <ChevronIcon src = {ChevrondownIconPath} alt ='Rotating-Chevron'></ChevronIcon>
+      </SelectWrapper>
+       <DropdownUl $currencyState= {this.state.currencySwitch}>
+          <CurrencyList>USD</CurrencyList>
+          <CurrencyList>EUR</CurrencyList>
+          <CurrencyList>JYP</CurrencyList>
+        </DropdownUl> 
     </Wrapper>
 
       
