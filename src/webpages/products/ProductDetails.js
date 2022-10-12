d26eb218716a2c5bfe4cdf36bc2aee2d638d5b43
CrucialE <erasmasNgandu@gmail.com>
2022-10-12 11:57:33 +0200

    Remove junior-react-endpoint

--- src/webpages/products/ProductDetails.js
+++ src/webpages/products/ProductDetails.js
@@ -16,47 +16,52 @@ const ProductDisplayLayout = styled.section`
 `;
 
 const ProductImage = styled.div`
-  height:511px;
-  height:610px;
-  object-fit:cover;
+  height: 511px;
+  height: 600px;
+  object-fit: cover;
   flex: 3;
-  
-  ;
- 
-   > img {
-    width:100%;
-    height:100%;
-    /* object-fit: cover; */
+
+  > img {
+    width: 100%;
+    height: 100%;
+    /* object-fit: cover; is put on the parent */
   }
 `;
 
 const ProductInfo = styled.article`
   flex: 2;
+  > h3 {
+    font-size: ${FONTS.FAMILIES.ROBOTO_CONDENSED};
+    font-weight: ${FONTS.WEIGHTS.LARGER};
+    font-size: ${FONTS.SIZES.EIGHTEEN};
+  }
 `;
 const Gallery = styled.aside`
-  display:flex;
-  align-items:flex-end;
-  flex-direction:column;
-   width:80px;
->img {
-  margin-bottom: 5px;
-  cursor:pointer;
-}
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  width: 80px;
+  > img {
+    margin-bottom: 5px;
+    cursor: pointer;
+  }
 `;
 
 const DescriptionText = styled.div`
   font-family: ${FONTS.FAMILIES.ROBOTO};
   font-weight: ${FONTS.WEIGHTS.MEDIUM};
-  font-size:${FONTS.SIZES.SIXTEEN};
+  font-size: ${FONTS.SIZES.SIXTEEN};
   color: ${COLORS.BLACK};
-  max-width:fit-content;
+  margin-top:18px;
+  max-width: fit-content; 
 `;
 const AddToCartBtn = styled.button`
+  margin-top: 14px;
   background-color: ${COLORS.GREEN};
   font-family: ${FONTS.FAMILIES.RALEWAY};
   font-size: ${FONTS.SIZES.SIXTEEN};
   font-weight: ${FONTS.WEIGHTS.LARGER};
-  margin-bottom:40px;
+  margin-bottom: 40px;
   border: none;
   width: 100%;
   padding: 10px;
@@ -64,10 +69,10 @@ const AddToCartBtn = styled.button`
   color: ${COLORS.WHITE};
   text-transform: uppercase;
 `;
-// const ProductTile = styled.aside`
-//   width: 79px;
-//   height: 80px;
-// `;
+const ProductTile = styled.img`
+  width: 79px;
+  height: 80px;
+`;
 const Size = styled.span`
   font-family: ${FONTS.FAMILIES.ROBOTO};
   font-size: ${FONTS.SIZES.TEN};
@@ -75,61 +80,56 @@ const Size = styled.span`
   width: 40px;
   height: 28px;
   display: inline-block;
-  margin-right:4px;
+  margin-right: 4px;
   text-align: center;
-  line-height:28px;
+  line-height: 28px;
   cursor: pointer;
-  
 
   &:hover {
     background-color: ${COLORS.BLACK};
     color: ${COLORS.WHITE};
   }
-`
+`;
 const ColorSwatch = styled.div`
-margin-top:18px;
-> h3{
-  font-family:${FONTS.FAMILIES.ROBOTO_CONDENSED};
-  font-weight:${FONTS.WEIGHTS.LARGER};
-  font-size:${FONTS.SIZES.EIGHTEEN};
-  margin-bottom:3px;
-}
->span{
-  display:inline-block;
-  height:36px;
-  width:36px;
-  margin-right:4px;
-	border:1px solid gray;
-	border-radius: 1px;
-  cursor:pointer;
-}
-`
+  margin-top: 18px;
+  > h3 {
+    font-family: ${FONTS.FAMILIES.ROBOTO_CONDENSED};
+    font-weight: ${FONTS.WEIGHTS.LARGER};
+    font-size: ${FONTS.SIZES.EIGHTEEN};
+    margin-bottom: 3px;
+  }
+  > span {
+    display: inline-block;
+    height: 36px;
+    width: 36px;
+    margin-right: 4px;
+    border: 1px solid gray;
+    border-radius: 1px;
+    cursor: pointer;
+  }
+`;
 const Price = styled.h3`
   font-family: ${FONTS.FAMILIES.ROBOTO_CONDENSED};
   font-weight: ${FONTS.WEIGHTS.LARGEST};
   font-size: ${FONTS.SIZES.EIGHTEEN};
-  margin:10px 0;
+  margin: 10px 0;
   line-height: 18px;
-  >span{
-    display:block;
-    margin:10px 0;
+  > span {
+    display: block;
+    margin: 10px 0;
   }
 `;
 export default class ProductDetails extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  state ={
-    selectedColor : '',
-    selectedSize : '',
-    galleryImagePosition: 0
-  }
+  state = {
+    selectedColor: "",
+    selectedSize: "",
+    galleryImagePosition: 0,
+  };
 
-  changeImage = position => {
-  this.setState({galleryImagePosition: position})
-  }
-  // }
+  changeImage = (position) => {
+    this.setState({ galleryImagePosition: position });
+  };
 
- 
   render() {
     const LOCATION_PARTS = window.location.pathname.split("/");
     // console.log(location)
@@ -147,40 +147,59 @@ export default class ProductDetails extends React.Component {
       <>
         <ProductDisplayLayout>
           <Gallery>
-            {
-              PRODUCT.gallery.map((image,index) => 
-                <img key={index.toString()} src={image} onClick={() => this.changeImage(index) } width="79" height="80"/>
-              )
-            }
-          </Gallery >
+            {PRODUCT.gallery.map((image, index) => (
+              <ProductTile
+                key={index.toString()}
+                src={image}
+                onClick={() => this.changeImage(index)}
+                alt="gallery-thumbnails"
+              />
+            ))}
+          </Gallery>
 
           <ProductImage>
-            <img src={PRODUCT.gallery[this.state.galleryImagePosition]} alt="chosen-item" />
+            <img
+              src={PRODUCT.gallery[this.state.galleryImagePosition]}
+              alt="chosen-item"
+            />
           </ProductImage>
           <ProductInfo>
             <h1>{PRODUCT.title}</h1>
             <h2>{PRODUCT.brand}</h2>
             <h3>Size:</h3>
             <p>
-              {PRODUCT.availableSizes.map((size) =>( <Size id={size} onClick={(event)=>{ this.setState({selectedSize:event.target.id}) 
-              setTimeout(()=>{
-                 console.log(this.state.selectedSize)
-                 },500)
-                   }} key={size}>{size}</Size>))
-              
-              }
-            
+              {PRODUCT.availableSizes.map((size) => (
+                <Size
+                  id={size}
+                  onClick={(event) => {
+                console.log(event.target.id );
+                    // setTimeout(() => {
+                    //   console.log(this.state.selectedSize);
+                    // }, 1000); tO SET STATE YOU NEED A KEY
+                  }}
+                  key={size}
+                >
+                  {size}
+                </Size>
+              ))}
             </p>
             <ColorSwatch>
               <h3>Color:</h3>
-              {PRODUCT.colors.map(color => {return <span key={color} style={{backgroundColor: color }} 
-                onClick={(event) => {this.setState({selectedColor: event.target.style.backgroundColor})
-                  setTimeout(()=>{
-                    console.log(this.state.selectedColor)
-                  },2000)
-                  
-                }
-              }></span>
+              {PRODUCT.colors.map((color) => {
+                return (
+                  <span
+                    key={color}
+                    style={{ backgroundColor: color }}
+                    onClick={(event) => {
+                      this.setState({
+                        selectedColor: event.target.style.backgroundColor,
+                      });
+                      setTimeout(() => {
+                        console.log(this.state.selectedColor);
+                      }, 2000);
+                    }}
+                  ></span>
+                );
 
                 //when the user clicks on a color swatch:
                 //1. Implement an event
@@ -189,16 +208,13 @@ export default class ProductDetails extends React.Component {
                 //4. Console the color to find out if it's the right color
                 //5. Then store in the appropriate state
               })}
-
             </ColorSwatch>
             <Price>
-             Price:<span>${PRODUCT.price.USD}</span>
+              Price:<span>${PRODUCT.price.USD}</span>
             </Price>
 
             <AddToCartBtn>Add To Cart</AddToCartBtn>
-            <DescriptionText>
-              {PRODUCT.description}
-            </DescriptionText>
+            <DescriptionText>{PRODUCT.description}</DescriptionText>
           </ProductInfo>
         </ProductDisplayLayout>
       </>
